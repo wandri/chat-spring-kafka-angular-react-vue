ef15322d602026f55b1ba5d2115964abb107b55b
wandri <wandrille.guesdon@gmail.com>
2019-03-27 14:14:16 +0100

    change front folder name by chat-front

---
+++ chat-front/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UserService } from './user/user.service';
+import { User } from './user/user.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('userInput')
+  userInput: ElementRef;
+
+  userName = new FormControl('');
+
+  user: User;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.userInput.nativeElement.focus());
+  }
+
+  activateUser() {
+    if ( this.userName.value && this.userName.value !== '' ) {
+      this.userService.getUser(this.userName.value)
+        .then(user => this.user = user);
+    }
+  }
+}
---
+++ chat-front/src/app/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user.interface';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private server = environment.server;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUser(name: string): Promise<User> {
+    return this.http.post<User>(`${this.server}/users`, {name}).toPromise();
+  }
+}
