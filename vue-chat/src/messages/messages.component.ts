20d8a851579dc8353cd33a815650f20d5eac8da3
wandri <wandrille.guesdon@gmail.com>
2019-03-29 10:13:17 +0100

    remove previous vue app and initiate new one.

--- vue-chat/src/messages/messages.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class HelloWorld extends Vue {
-    @Prop() private msg!: string;
-}
--- vue-chat/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        data: '@import "@/scss/settings.scss";'
-      }
-    }
-  }
-}
