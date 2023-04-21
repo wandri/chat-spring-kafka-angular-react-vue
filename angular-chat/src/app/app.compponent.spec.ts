c32ce97a37e4fdf02423444b00505ba9276c95e1
Wandrille <15016833+wandri@users.noreply.github.com>
2023-04-21 15:56:03 +0200

    Fix test angular.json

---
+++ angular-chat/src/app/app.compponent.spec.ts
@@ -0,0 +1,21 @@
+import { AppComponent } from "./app.component";
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
+describe("AppComponent", () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [
+        AppComponent
+      ]
+    }).compileComponents();
+  });
+
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
