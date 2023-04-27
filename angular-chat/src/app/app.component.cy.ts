5236e8808747916e347991388d26e04a5faa1a88
Wandrille <15016833+wandri@users.noreply.github.com>
2023-04-27 09:13:16 +0200

    Create cypress unit tests for app.component.ts

--- angular-chat/src/app/app.component.cy.ts
+++ angular-chat/src/app/app.component.cy.ts
@@ -1,18 +1,24 @@
 import { AppComponent } from "./app.component";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { UserService } from "./user/user.service";
 import { MessagesComponent } from "./messages/messages.component";
-import { MockComponent } from "ng-mocks";
+import { MockComponent, MockProvider } from "ng-mocks";
+import { User } from "./user/user.interface";
 
-const imports = [CommonModule, FormsModule, ReactiveFormsModule, HttpClientTestingModule];
-const providers = [UserService];
+const user: User = {
+  name: "michael",
+  id: "some-id"
+};
+
+const imports = [CommonModule, FormsModule, ReactiveFormsModule];
+const providers = [MockProvider(UserService, { getUser: () => Promise.resolve(user) })];
 const config = {
   providers,
   imports,
   declarations: [MockComponent(MessagesComponent)]
 };
+
 describe("App component", () => {
   it("mounts", () => {
     cy.mount(AppComponent, config);
@@ -23,6 +29,11 @@ describe("App component", () => {
     cy.get("app-messages").should("not.exist");
   });
 
+  it("The input should be focused by default", () => {
+    cy.mount(AppComponent, config);
+    cy.get("input").should("be.focused");
+  });
+
   it("should display the action button when a name is written", () => {
     cy.mount(AppComponent, config);
     cy.get(`button[type="submit"]`).should("not.exist");
@@ -30,19 +41,24 @@ describe("App component", () => {
     cy.get(`button[type="submit"]`).should("exist");
   });
 
-  it("should send a request with the name when clicking on the action button", () => {
+  describe("When clicking on the action button, when the name is filled", () => {
     const name = "Mike";
-    const user = {
-      user: "michael",
-      id: "some-id"
-    };
-    cy.mount(AppComponent, config).then((wrapper) => {
-      cy.stub(wrapper.component.userService, "getUser", () => Promise.resolve(user)).as("getUser");
-      return cy.wrap(wrapper).as("angular");
+    beforeEach(() => {
+      cy.mount(AppComponent, config).then((wrapper) => {
+        cy.spy((wrapper.component as any).userService, "getUser").as("getUser");
+        return cy.wrap(wrapper).as("angular");
+      });
+      cy.get("input").type(name);
+      cy.get(`button[type="submit"]`).click();
     });
-    cy.get("input").type(name);
-    cy.get(`button[type="submit"]`).click();
-    cy.get(`@getUser`).should("have.been.calledWith", name);
-    cy.get(`app-messages`).should("exist");
+
+    it("should send a request with the name ", () => {
+      cy.get(`@getUser`).should("have.been.calledWith", name);
+    });
+
+    it("should not display the user form but the message instead", () => {
+      cy.get(`app-messages`).should("exist");
+      cy.get(`.userForm`).should("not.exist");
+    });
   });
 });
--- angular-chat/src/app/app.compponent.spec.ts
+++ angular-chat/src/app/app.compponent.spec.ts
@@ -15,6 +15,7 @@ describe("AppComponent", () => {
   it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // @ts-ignore
     expect(app).toBeTruthy();
   });
 
